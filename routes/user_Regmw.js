de0143be989059bb575a15aaa2b361f45e28d9f2
Adarsh Rao <Rravi1347@gmail.com>
2020-11-08 14:47:15 +0530

    nov8 commit

--- routes/user_Regmw.js
+++ routes/user_Regmw.js
@@ -3,6 +3,7 @@ let router = express();
 let user_reg = require('../schema/userReg');
 let bcrypt = require('bcrypt');
 
+//creating user
 router.post('/register', async(req,res)=>{
     let result = user_reg.validationError(req.body);
     if(result.error)
@@ -27,5 +28,49 @@ router.post('/register', async(req,res)=>{
     await newcustomer.save();
     res.send({message:'registration sucessful'});
 });
+
+
+//updating user
+router.put('/update/:id',async(req,res)=>{
+    let findobj = await user_reg.user.findById(req.params.id);
+    if(!findobj)
+    {
+      return res.status(404).send({message:'Invalid id'}); 
+    }
+     
+  let result = user_reg.validationError(req.body);
+  console.log(result);
+  if(result.error)
+  {
+      return res.status(400).send(result.error.details[0].message)
+  }
+    //findobj.name = req.body.name;
+    findobj.firstname= req.body.firstname,
+    findobj.lastname=req.body.lastname,
+    findobj.userlogin=req.body.userlogin;
+    //encrypt password
+    let salt = await bcrypt.genSalt(10);
+    findobj.userlogin.password = await bcrypt.hash(findobj.userlogin.password,salt);
+
+    await findobj.save();
+    res.send({message:'user updated'});
+  })
+
+  //for delete operation
+  router.delete('/delete/:id',async(req,res)=>{
+    let findobj = await user_reg.user.findByIdAndRemove(req.params.id);
+    if(!findobj)
+    {
+      return res.status(404).send({message:'Invalid id'}); 
+    }
+    res.send({message:'user deleted'});
+});
+
+//to get all users
+router.get('/data',async(req,res)=>{
+  let customerdata = await user_reg.user.find();
+  //model mese table name milega aur uss table me kitne collections hai wo milenge.
+   res.send(customerdata);
+   }); 
 
 module.exports = router;
