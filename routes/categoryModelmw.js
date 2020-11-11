3db14cf2533a66e953180d000ba36f42a8455078
Adarsh Rao <Rravi1347@gmail.com>
2020-11-11 15:17:38 +0530

    11nov commit

--- routes/categoryModelmw.js
+++ routes/categoryModelmw.js
@@ -68,4 +68,87 @@ for (var i = 0; i < req.body.subcategory.length; i++)
    
 });
 
+//show all categories
+router.post('/showCategories/:page',async(req,res)=>{
+    let perpage = 4;
+    let currentpage = req.params.page || 1;
+    let data = await catmodel.category.find()
+                                .skip((perpage * currentpage) - perpage)
+                                .limit(perpage);
+   let totalcount = await catmodel.category.find().count();
+   let totalpages = Math.ceil(totalcount/perpage);
+   res.send({
+    perpage: perpage,
+    currentpage: currentpage,
+    data: data,
+    totalcount: totalcount,
+    totalpages:totalpages
+   });
+
+});
+
+//update categories
+router.put('/updateCat/:id',async(req,res)=>{
+    let findobj = await catmodel.category.findById(req.params.id);
+    if(!findobj)
+    {
+      return res.status(404).send({message:'Invalid id'}); 
+    }
+     
+  let result = catmodel.validationError(req.body);
+  if(result.error)
+  {
+      return res.status(400).send(result.error.details[0].message)
+  }
+
+    findobj.categoryName= req.body.categoryName;
+    for (var i = 0; i < findobj.subcategory.length; i++) 
+  {
+    findobj.subcategory[i].catName=req.body.categoryName;
+  }
+    //findobj.subcategory[0].catName=req.body.categoryName
+    //findobj.subcategory.catName=req.body.categoryName,
+    //console.log(findobj.subcategory[0].catName);
+
+   await findobj.save();
+   console.log(findobj.subcategory[0].catName);
+   res.send({message:'Category updated,please change the catName in subcategory table.'});
+
+   //logic incomplete.
+//    // simultanous updation of subcategory catName
+//    for (var i = 0; i < findobj.subcategory.length; i++) 
+//    {
+//       let findobj1 =await subcatmodel.subcategory.findById(findobj.subcategory[i]._id); 
+//       if(!findobj1)
+//        {
+//           return res.status(404).send({message:'Invalid id'}); 
+//        }
+    
+//         let result1 = subcatmodel.validationError(req.body);
+//         console.log(result1);
+//         if(result1.error)
+//           {
+//              return res.status(400).send(result1.error.details[0].message)
+//           }
+   
+//          findobj.catName=req.body.categoryName
+
+//    await findobj.save();
+//    res.send({message:'subcategory '+i+' updated'});
+// }
+  });
+
+
+
+//for delete subcategory
+router.delete('/deleteCat/:id',async(req,res)=>{
+    let findobj = await catmodel.category.findByIdAndRemove(req.params.id);
+    if(!findobj)
+    {
+      return res.status(404).send({message:'Invalid id'}); 
+    }
+    res.send({message:'category deleted'});
+});
+   
+
 module.exports = router;
